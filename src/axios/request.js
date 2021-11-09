7018d63076386fe4cd0061d8013d1a1658f40190
Roshar <rashid.batukaev@gmail.com>
2021-11-09 15:23:22 +0300

    NEW DB FOR LOCAL VDS

--- src/axios/request.js
+++ src/axios/request.js
@@ -4,7 +4,8 @@ import router from '../router'
 
 const requestAxios = axios.create({
     // baseURL: process.env.VUE_APP_URL
-    baseURL: 'https://it-govzalla-back.onrender.com' 
+    // baseURL: 'https://it-govzalla-back.onrender.com'
+       baseURL: 'http://193.168.46.58:8081'
 })
 
 requestAxios.interceptors.response.use(null,error => {
