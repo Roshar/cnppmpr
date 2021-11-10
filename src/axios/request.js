433b291a2045c2ed409436fa0c764a90fb28292a
Roshar <rashid.batukaev@gmail.com>
2021-11-10 12:19:13 +0300

    10 Nov 2021 -3

--- src/axios/request.js
+++ src/axios/request.js
@@ -5,7 +5,7 @@ import router from '../router'
 const requestAxios = axios.create({
     // baseURL: process.env.VUE_APP_URL
     // baseURL: 'https://it-govzalla-back.onrender.com'
-       baseURL: 'http://193.168.46.58:8088'
+       baseURL: 'http://it-govzalla.ru'
     //    baseURL: 'http://it-govzalla.ru'
 })
 
