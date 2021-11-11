50fa5e04603349d7a90c7635cf66b9530ee838d8
Roshar <rashid.batukaev@gmail.com>
2021-11-11 16:01:50 +0300

    rename group to group_

--- src/views/tutor/student/profile.vue
+++ src/views/tutor/student/profile.vue
@@ -65,7 +65,7 @@
                                                 <path d="M8.5 2.687c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z"/>
                                             </svg>
                                                 ИОМ</h6>
-                                            <span class="text-secondary" v-if="iom"><router-link :to="{path:`/iom/${issetIom.iom_id}/exercise`}">{{issetIom.title}}</router-link></span>
+                                            <span class="text-secondary" v-if="iom.length"><router-link :to="{path:`/iom/${issetIom.iom_id}/exercise`}">{{issetIom.title}}</router-link></span>
                                             <span class="text-secondary" v-else>{{issetIom}}</span>
                                         </li>
                                     </ul>
@@ -133,7 +133,7 @@
                                         <hr>
                                     </div>
                                 </div>
-                                <div v-if="iom">
+                                <div v-if="iom.length">
                                     <div class="row gutters-sm">
                                         <div class="col-sm-12 mb-6">
                                             <div class="card h-100">
@@ -289,30 +289,29 @@
                 loading.value = true
                 const userId = route.params.userId
                 profile.value = await store.dispatch('admin/getProfile',{tbl:'students', userId:userId})
-                // await store.dispatch('admin/getDependenciesStudent',{ userId:userId})
-                // dependencies.value = store.getters['admin/getDependenciesData']
-                //
-                // //DEPENDENCIES INFO
-                // if(dependencies.value.length) {
-                //     groupName.value  = dependencies.value[0]['title']
-                //     tutorFio.value = dependencies.value[0]['surname'] + ' '+ dependencies.value[0]['name'][0]+'.'+dependencies.value[0]['patronymic'][0]+'.'
-                //     tutorId.value = dependencies.value[0]['t_user_id']
-                //     groupId.value = dependencies.value[0]['group_id']
-                //     iom.value = await store.dispatch('admin/getIomByStudentAndTutor',
-                //         { student:userId,
-                //             tutor:tutorId.value})
-                //     issetIom.value = iom.value.length ? iom.value[0] : 'не назначен'
-                //
-                //     if(iom.value.length) {
-                //         await store.dispatch('iom/getExercisesByIomId',
-                //             {token:localStorage.getItem('jwt-token'),id:iom.value[0].iom_id})
-                //         iomInfo.value = store.getters['iom/getExercisesByIomId']
-                //         await store.dispatch('iom/getStatusFinished',{studentId:userId,iomId:iom.value[0].iom_id})
-                //         finishedExercises.value = store.getters['iom/getStatusFinished']
-                //
-                //
-                //     }
-                // }
+                dependencies.value = await store.dispatch('admin/getDependenciesStudent',{ userId:userId})
+
+                //DEPENDENCIES INFO
+                if(dependencies.value.length) {
+                    groupName.value  = dependencies.value[0]['title']
+                    tutorFio.value = dependencies.value[0]['surname'] + ' '+ dependencies.value[0]['name'][0]+'.'+dependencies.value[0]['patronymic'][0]+'.'
+                    tutorId.value = dependencies.value[0]['t_user_id']
+                    groupId.value = dependencies.value[0]['group_id']
+                    iom.value = dependencies.value = await store.dispatch('admin/getIomByStudentAndTutor',
+                        { student:userId,
+                            tutor:tutorId.value})
+                    issetIom.value = iom.value.length ? iom.value[0] : 'не назначен'
+
+                    if(iom.value.length) {
+                        await store.dispatch('iom/getExercisesByIomId',
+                            {token:localStorage.getItem('jwt-token'),id:iom.value[0].iom_id})
+                        iomInfo.value = store.getters['iom/getExercisesByIomId']
+                        await store.dispatch('iom/getStatusFinished',{studentId:userId,iomId:iom.value[0].iom_id})
+                        finishedExercises.value = store.getters['iom/getStatusFinished']
+
+
+                    }
+                }
                 //USER INFO
                 surname.value = profile.value[0].surname
                 currentUser.value = profile.value[0]['user_id']
