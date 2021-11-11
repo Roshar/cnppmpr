26cf87786d171bc36823450230df35eee8ab452f
Roshar <rashid.batukaev@gmail.com>
2021-11-11 11:09:21 +0300

    TEST 1.1

--- vue.config.js
+++ vue.config.js
@@ -3,13 +3,14 @@
 //         socket: 'socket',
 //         disableHostCheck: true,
 //         port: 8080,
-//         public: '192.168.0.102:8080',
+//         public: '193.168.46.58:8080',
 //         // https: true,
 //     },
 //     publicPath: "/"
 // }
-module.exports = {
-    devServer: {
-        disableHostCheck: true
-    }
-}
+// module.exports = {
+// 193.168.46.58
+//     devServer: {
+//         disableHostCheck: true
+//     }
+// }
