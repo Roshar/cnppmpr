635a95c1704c4ec3134abb0e9d9d98de52826115
Roshar <rashid.batukaev@gmail.com>
2021-11-25 10:49:24 +0300

    25 Nov 21 error2

--- src/views/admin/student/last.vue
+++ src/views/admin/student/last.vue
@@ -3,8 +3,8 @@
         <admin-student-menu></admin-student-menu>
     </div>
     <div class="col-9">
-        <app-loader v-if="loading"></app-loader>
-        <div class="content-wallpaper" v-else>
+<!--        <app-loader v-if="loading"></app-loader>-->
+        <div class="content-wallpaper">
             <h5 >Последние зарегистрировавшиеся слушатели </h5>
 
             <div class="row">
@@ -41,11 +41,11 @@
                         <td> <input :class="btnActiveClass" :disabled="disabled" type="button" @click="activation(item.user_id)" :value="btnActiveValue">  </td>
                         <td>{{item.created}}</td>
                         <td>
-<!--                            <div v-if="deactivation" style="text-align: center" @click="deactivationUser(item.user_id)">-->
-<!--                                <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-person-x-fill" viewBox="0 0 16 16">-->
-<!--                                    <path fill-rule="evenodd" d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm6.146-2.854a.5.5 0 0 1 .708 0L14 6.293l1.146-1.147a.5.5 0 0 1 .708.708L14.707 7l1.147 1.146a.5.5 0 0 1-.708.708L14 7.707l-1.146 1.147a.5.5 0 0 1-.708-.708L13.293 7l-1.147-1.146a.5.5 0 0 1 0-.708z"/>-->
-<!--                                </svg>-->
-<!--                            </div>-->
+                            <div v-if="deactivation" style="text-align: center" @click="deactivationUser(item.user_id)">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-person-x-fill" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm6.146-2.854a.5.5 0 0 1 .708 0L14 6.293l1.146-1.147a.5.5 0 0 1 .708.708L14.707 7l1.147 1.146a.5.5 0 0 1-.708.708L14 7.707l-1.146 1.147a.5.5 0 0 1-.708-.708L13.293 7l-1.147-1.146a.5.5 0 0 1 0-.708z"/>
+                                </svg>
+                            </div>
                         </td>
                     </tr>
                 </tbody>
@@ -112,19 +112,19 @@
             }
 
             onMounted(async()=>{
-                loading.value = true
+                // loading.value = true
                 // STUDENT INFO
                 areas.value = await store.dispatch('area/getAreas')
                 disciplines.value = await store.dispatch('discipline/getDisciplines')
                 lastStudents.value = await store.dispatch('admin/getLastUsers',{tbl:'students'})
                 console.log(lastStudents.value)
                 //AREA INFO
-                loading.value = false
+                // loading.value = false
             })
 
 
             return {
-                loading,
+                // loading,
                 areas,
                 disciplines,
                 students,
