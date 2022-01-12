5edb425cd1796fbd2cde626dbad53a12cbc10662
Roshar <rashid.batukaev@gmail.com>
2022-01-12 19:44:42 +0300

    12 Jan 2022 ---3

--- src/components/request/RequestTask.vue
+++ src/components/request/RequestTask.vue
@@ -2,10 +2,10 @@
     <div class="card">
         <div class="card-body">
             <span style="font-style: italic;">Наименование: </span><h5 class="card-title">{{taskData.title}}</h5>
-            <span style="font-style: italic;">Описание: </span> <div v-html="taskData.description"></div>
+            <span style="font-style: italic;">Описание: </span> <div v-if="taskData.description" v-html="taskData.description"></div> <div v-else> Пусто</div>
             <br>
             <span style="font-style: italic;">Автор:  </span><p class="card-text">{{currentMentor}}</p>
-            <span style="font-style: italic;">Ссылка: </span><p class="card-text">{{taskData.link}}</p>
+            <span style="font-style: italic;">Ссылка: </span> <p v-if="taskData.link" class="card-text"><a   href="{{taskData.link}}">{{taskData.link}}</a> </p> <p v-else class="card-text">Пусто</p>
             <span style="font-style: italic;">Категория: </span><p class="card-text">{{taskData['title_tag']}}</p>
             <span style="font-style: italic;">Уровень: </span><p class="card-text">{{taskData['level_title']}}</p>
             <span style="font-style: italic;">Срок выполнения: </span><p class="card-text">{{checkTerm(taskData['term'], taskData['term'])}}</p>
